2f54d9bf47c492dd39985a6de8b2b58340592a3b
jaehyeon2650 <ind07152@naver.com>
2023-07-21 15:28:55 +0900

    fix6

--- src/middleware.js
+++ src/middleware.js
@@ -3,19 +3,20 @@ import multers3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 import aws from "aws-sdk";
 
-// const s3 = new aws.S3({
-//     credentials: {
-//         accessKeyId: process.env.AWS_ID,
-//         secretAccessKey: process.env.AWS_SECRET,
-//     },
-// });
-const s3 = new S3Client({
+const s3 = new aws.S3({
     region: "us-east-2",
     credentials: {
         accessKeyId: process.env.AWS_ID,
         secretAccessKey: process.env.AWS_SECRET,
     },
 });
+// const s3 = new S3Client({
+//     region: "us-east-2",
+//     credentials: {
+//         accessKeyId: process.env.AWS_ID,
+//         secretAccessKey: process.env.AWS_SECRET,
+//     },
+// });
 
 // const isHeroku = process.env.NODE_ENV === "production";
 // const s3imageUploader = multers3({
